7640eb96e3af8f28aa67a4f7466454d37e42973d
ctrachte <cetrachte@hotmail.com>
2020-01-14 18:46:38 -0600

    fix syntax errors

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,9 +1,10 @@
 describe('Address Book', function () {
     it('should be able to add a new contact', function () {
-      let addressBook = new AddressBook(), new Contact ();
+      let addressBook = new AddressBook();
+      let thisContact = new Contact();
       //add a contact to the book.
       addressBook.addContact(thisContact);
 
       expect(addressBook.getContact(0)).toBe(thisContact);
     })
-)}
+})
