2d68782f351d64aca377606a1c4341a8dc4f114b
ctrachte <cetrachte@hotmail.com>
2020-01-14 18:27:36 -0600

    add documentation for `matcher`

--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -13,7 +13,8 @@ describe("Player", function() {
   it("should be able to play a Song", function() {
     player.play(song);
     expect(player.currentlyPlayingSong).toEqual(song);
-
+    //`toEqual` Is an example of a 'matcher'
+    // matchers are used for comparison of values to see if a spec passes
     //demonstrates use of custom matcher
     expect(player).toBePlaying(song);
   });
