58b905105b9ad85b2d20ccb34d0fcc1090793ba2
ctrachte <cetrachte@hotmail.com>
2020-01-14 18:59:47 -0600

    changes to reduce redundancy

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,16 +1,20 @@
 describe('Address Book', function () {
+  let addressBook, thisContact;
+
+  //reduce redundancy by adding all repeated code to a `beforeEach()`
+  beforeEach(function() {
+     addressBook = new AddressBook();
+     thisContact = new Contact();
+  });
+
     it('should be able to add a new contact', function () {
-      let addressBook = new AddressBook();
-      let thisContact = new Contact();
       //add a contact to the book.
       addressBook.addContact(thisContact);
-
       expect(addressBook.getContact(0)).toBe(thisContact);
     });
 
     it('should be able to delete a contact', function () {
-      let addressBook = new AddressBook();
-      let thisContact = new Contact();
+
       //add a contact to the book.
       addressBook.addContact(thisContact);
       // test deleting a contact
